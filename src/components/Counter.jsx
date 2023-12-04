21d8786619b85cf7bb0fdf381b34689874b59bbf
OlehBrov <o.dudnyk85@gmail.com>
2023-12-04 19:27:16 +0200

    upd readme

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,17 +1,14 @@
-import { useEffect, useState } from "react";
+
 import CountUp from "react-countup";
 
 export const Counter = () => {
-  const [start, setStart] = useState(10000000);
-  const [end, setEnd] = useState(1134147814);
-  const [duration, setDuration] = useState(3)
  
   return (
     <CountUp
       startOnMount={false}
-      start={start}
-      end={end}
-      duration={duration}
+      start={10000000}
+      end={1134147814}
+      duration="2"
       separator=" "
       enableScrollSpy={true}
       scrollSpyDelay={300}
@@ -19,7 +16,7 @@ export const Counter = () => {
       
       redraw={true}
     >
-      {({ countUpRef, update }) => (
+      {({ countUpRef }) => (
         <div className="counter-wrap">
           <span className="counter" ref={countUpRef} />
         </div>
