dacc418d8df46e45f578fb6d3a021d0b8de7de37
OlehBrov <o.dudnyk85@gmail.com>
2023-12-04 18:32:56 +0200

    burger mobile view fix

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,23 +1,25 @@
-
+import { useEffect, useState } from "react";
 import CountUp from "react-countup";
 
 export const Counter = () => {
-  
-  
-
+  const [start, setStart] = useState(10000000);
+  const [end, setEnd] = useState(1134147814);
+  const [duration, setDuration] = useState(3)
+ 
   return (
     <CountUp
       startOnMount={false}
-      start={100000}
-      end={1134147814}
-      duration={2.75}
+      start={start}
+      end={end}
+      duration={duration}
       separator=" "
       enableScrollSpy={true}
       scrollSpyDelay={300}
       scrollSpyOnce={true}
-
+      
+      redraw={true}
     >
-      {({ countUpRef }) => (
+      {({ countUpRef, update }) => (
         <div className="counter-wrap">
           <span className="counter" ref={countUpRef} />
         </div>
